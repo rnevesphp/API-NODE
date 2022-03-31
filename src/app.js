e728745737e54379c356ab3884abdfc0989d4886
Romeu <romeu.esp@gmail.com>
2022-03-31 15:20:17 +0200

    :checkered_flag: Bodyparser package added into the project

--- src/app.js
+++ src/app.js
@@ -4,12 +4,20 @@
  * importamos o express
  */
 const express = require('express'); 
+const bodyParser = require('body-parser');
+const { urlencoded } = require('express');
 
 /**
  * declaramos o app e a o router (aplicação principal e rota)
  */
 const app = express(); 
 const router = express.Router(); 
+
+/**
+ * bodyParse => pacote do node 
+ */
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false })); 
 
 /**
  * criando a rota
@@ -22,9 +30,16 @@ const route = router.get('/' , (req, res, next) => {
 }); 
 
 /**
+ * rota para criar ou enviar dados ao bando de dados
+ */
+const create = router.post('/' , (req, res, next) => {
+   res.status(200).send(req.body); 
+}); 
+/**
  * atribuimos a rota à aplicação. 
  */
 app.use('/',route); 
+app.use('/products' , create)
 
 /**
  * Exportar a aplicação. 
